0d18fa735b43a5c2546eaad5164b034e7cc61e6c
Juan Carlos <quintanilla.peru@gmail.com>
2015-09-23 14:24:41 -0500

    ajustes para el html

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,14 +2,21 @@
 
 class HomeController extends BaseController {
 
+	private $_uri;
+	
+	public function __construct()
+	{
+		$this->_uri = Request::path();
+	}
+	
 	public function getInicio()
 	{
-		return View::make('inicio')->with('clase', 'inicio');
+		return View::make('inicio')->with('clase', 'inicio')->with('uri', $this->_uri);
 	}
 	
 	public function getIlRistorante()
 	{
-		return View::make('il-ristorante')->with('clase', 'il-ristorante');
+		return View::make('il-ristorante')->with('clase', 'il-ristorante')->with('uri', $this->_uri);
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,4 @@
 <?php
 
 Route::get('/', array('as' => 'inicio', 'uses' => 'HomeController@getInicio'));
-Route::get('/il-ristorante', array('as' => 'inicio', 'uses' => 'HomeController@getIlRistorante'));
+Route::get('/il-ristorante', array('as' => 'il-ristorante', 'uses' => 'HomeController@getIlRistorante'));
